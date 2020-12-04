8148450b86bcdacedf10ad4219f6f5584fd831b6
Hampus Gustafsson <1992hg@gmail.com>
2020-12-04 14:46:36 +0100

    Day 4

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod pipe;
+use regex::Regex;
+use std::collections::HashMap;
 
 fn reader() -> pipe::PipeReader {
     pipe::PipeReader {}
@@ -13,10 +15,236 @@ fn main() {
             "day2_2" => day2_2(),
             "day3_1" => day3_1(),
             "day3_2" => day3_2(),
+            "day4_1" => day4_1(),
+            "day4_2" => day4_2(),
             _ => println!("No solution for puzzle: {}", puzzle),
         },
         None => println!("No input..."),
     }
+}
+
+fn validate_fields(passport: &String) -> bool {
+    let mut validation: HashMap<&str, Box<dyn Fn(&&str) -> bool>> = HashMap::new();
+    validation.insert(
+        "byr",
+        Box::new(|value| -> bool {
+            if value.len() != 4 {
+                return false;
+            }
+            match value.parse::<i32>() {
+                Ok(r) => {
+                    if r >= 1920 && r <= 2002 {
+                        return true;
+                    }
+                }
+                Err(e) => println!("ERROR: [BYR] {}", e),
+            }
+            println!("  - BYR invalid {}", value);
+            return false;
+        }),
+    );
+    validation.insert(
+        "iyr",
+        Box::new(|value| -> bool {
+            if value.len() != 4 {
+                return false;
+            }
+            match value.parse::<i32>() {
+                Ok(r) => {
+                    if r >= 2010 && r <= 2020 {
+                        return true;
+                    }
+                }
+                Err(e) => println!("ERROR: [IYR] {}", e),
+            }
+            println!("  - IYR invalid {}", value);
+            return false;
+        }),
+    );
+    validation.insert(
+        "eyr",
+        Box::new(|value| -> bool {
+            if value.len() != 4 {
+                return false;
+            }
+            match value.parse::<i32>() {
+                Ok(r) => {
+                    if r >= 2020 && r <= 2030 {
+                        return true;
+                    }
+                }
+                Err(e) => println!("ERROR: [EYR] {}", e),
+            }
+            println!("  - EYR invalid {}", value);
+            return false;
+        }),
+    );
+    validation.insert(
+        "hgt",
+        Box::new(|value| -> bool {
+            if value.len() < 4 {
+                return false;
+            }
+            let chars: Vec<char> = value.chars().collect();
+            let unit: String = chars[chars.len() - 2..].into_iter().collect();
+            let height: String = value.chars().take(value.len() - 2).collect();
+            if unit == "in" {
+                match height.parse::<i32>() {
+                    Ok(r) => {
+                        if r >= 59 && r <= 76 {
+                            return true;
+                        }
+                    }
+                    Err(e) => println!("ERROR: [HGT] {}", e),
+                }
+            } else if unit == "cm" {
+                match height.parse::<i32>() {
+                    Ok(r) => {
+                        if r >= 150 && r <= 193 {
+                            return true;
+                        }
+                    }
+                    Err(e) => println!("ERROR: [HGT] {}", e),
+                }
+            }
+            println!("  - HGT invalid {}", value);
+            return false;
+        }),
+    );
+    validation.insert(
+        "hcl",
+        Box::new(|value| -> bool {
+            let valid = Regex::new("^#[0-9a-f]{6}$").unwrap().is_match(value);
+            if !valid {
+                println!("  - HCL invalid {}", value);
+            }
+            return valid;
+        }),
+    );
+    validation.insert(
+        "ecl",
+        Box::new(|value| -> bool {
+            let valid = Regex::new("^(amb|blu|brn|gry|grn|hzl|oth)$")
+                .unwrap()
+                .is_match(value);
+            if !valid {
+                println!("  - ECL invalid {}", value);
+            }
+            return valid;
+        }),
+    );
+    validation.insert(
+        "pid",
+        Box::new(|value| -> bool {
+            let valid = Regex::new("^[0-9]{9}$").unwrap().is_match(value);
+            if !valid {
+                println!("  - PID invalid {}", value);
+            }
+            return valid;
+        }),
+    );
+    let fields: Vec<Vec<&str>> = passport
+        .split(" ")
+        .map(|key_value| return key_value.split(":").collect())
+        .collect();
+    let mut valid = true;
+    for f in &fields {
+        if &f[0] == &"cid" {
+            continue;
+        }
+        match validation.get(&f[0]) {
+            Some(validation_func) => {
+                if !validation_func(&f[1]) {
+                    valid = false;
+                }
+            }
+            None => valid = false,
+        }
+    }
+    return valid;
+}
+
+fn has_fields(passport: &String) -> bool {
+    let valid_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    let fields: Vec<&str> = passport
+        .split(" ")
+        .map(|key_value| return &key_value[..3])
+        .collect();
+    for i in 0..valid_fields.len() {
+        let mut valid = false;
+        for f in &fields {
+            if *f == valid_fields[i] {
+                valid = true;
+                break;
+            }
+        }
+        if !valid {
+            println!("  - missing field {}", valid_fields[i]);
+            return false;
+        }
+    }
+    return true;
+}
+
+fn day4_2() {
+    let mut valid_count: i32 = 0;
+    loop {
+        let mut passport: String = String::new();
+        for line in reader() {
+            if line == "\n" || line == "" {
+                break;
+            }
+            if passport.len() > 0 {
+                passport.push_str(" ");
+            }
+            passport.push_str(line.trim());
+        }
+
+        if passport.len() == 0 {
+            break;
+        }
+
+        println!("{}", passport);
+        if has_fields(&passport) && validate_fields(&passport) {
+            valid_count += 1;
+            println!("  - VALID");
+        } else {
+            println!("  - INVALID");
+        }
+        passport.clear();
+    }
+
+    println!("Valid: {}", valid_count);
+}
+
+fn day4_1() {
+    let mut valid_count: i32 = 0;
+    loop {
+        let mut passport: String = String::new();
+        for line in reader() {
+            if line == "\n" || line == "" {
+                break;
+            }
+            if passport.len() > 0 {
+                passport.push_str(" ");
+            }
+            passport.push_str(line.trim());
+        }
+
+        if passport.len() == 0 {
+            break;
+        }
+
+        if has_fields(&passport) {
+            valid_count += 1;
+            println!("OK {}", passport);
+        } else {
+            println!("FAIL {}", passport);
+        }
+        passport.clear();
+    }
+
+    println!("Valid: {}", valid_count);
 }
 
 fn traverse(woods: &Vec<Vec<char>>, right: usize, down: usize, tree: char) -> usize {
